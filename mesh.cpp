da9d75ea25748fc0d3e2734032507d2f57452c80
zhaikr <1119391474@qq.com>
2022-10-11 17:05:44 +0800

    add function in mesh.cpp

--- mesh.cpp
+++ mesh.cpp
@@ -1,5 +1,43 @@
 #include "mesh.h"
 
+Mesh& Mesh::operator=(const Mesh& mesh)
+{
+    if (&mesh == this)
+    {
+        return *this;
+    }
+    VBO_ = mesh.VBO_;
+    EBO_ = mesh.EBO_;
+    return *this;
+}
+Mesh& Mesh::operator+=(const Mesh& mesh)
+{
+    AddMesh(mesh);
+    return *this;
+}
+
+void Mesh::AddMesh(const Mesh& mesh)
+{
+    int offset = VBO_.size();
+    VBO_.insert(VBO_.end(), mesh.VBO_.begin(), mesh.VBO_.end());
+    EBO_.reserve(EBO_.size() + mesh.EBO_.size());
+    for (int i = 0; i < mesh.EBO_.size(); i++) {
+        EBO_.emplace_back(mesh.EBO_[i] + offset);
+    }
+}
+
+void Mesh::AddTriangle(const Vertex& v1, const Vertex& v2, const Vertex& v3)
+{
+    int offset = VBO_.size();
+    VBO_.emplace_back(v1);
+    VBO_.emplace_back(v2);
+    VBO_.emplace_back(v3);
+    EBO_.emplace_back(offset);
+    EBO_.emplace_back(offset+1);
+    EBO_.emplace_back(offset+2);
+
+}
+
 void Mesh::DrawMesh()
 {
     SoftRenderCore::GetInstance().VBO_ = VBO_;
--- mesh.h
+++ mesh.h
@@ -8,6 +8,14 @@ class Mesh
 {
 public:
     Mesh() = default;
+    ~Mesh() = default;
+    Mesh(const Mesh& mesh)
+      :VBO_(mesh.VBO_), EBO_(mesh.EBO_){};
+    Mesh& operator=(const Mesh& mesh);
+    Mesh& operator+=(const Mesh& mesh);
+
+    void AddMesh(const Mesh& mesh);
+    void AddTriangle(const Vertex& v1, const Vertex& v2, const Vertex& v3);
     void DrawMesh();
     void MeshUpdata(const std::vector<unsigned int>& EBO_, const std::vector<Vertex>& VBO_);
 private:
